8a2568e5ef2cf45ebe42efe65735482c51db6aac
Jason Plumb <jplumb@splunk.com>
2021-09-14 16:20:36 -0700

    fix up results script

--- provision/results.sh
+++ provision/results.sh
@@ -4,12 +4,12 @@
 
 source env.sh
 
-FIELD_SEQ=$(for f in `seq 0 15` ; do for a in `seq 0 5` ; do echo -n "\$$(($f+2+15*$a)) \",\" "; done; done )
+FIELD_SEQ=$(for f in `seq 0 15` ; do for a in `seq 0 5` ; do echo -n "\$$(($f+2+16*$a)) \",\" "; done; done )
 AWKPROG=$(echo "{ print \$1 \",\" ${FIELD_SEQ} }" | sed -e "s/.....}/}/")
 
 for dir in $( ssh splunker@${TESTBOX_HOST} ls results) ; do
-  rsync -avv --progress splunker@${TESTBOX_HOST}:./results/$dir/results.csv results_${dir}.tmp.csv
-  echo awk -F, ${AWKPROG} results_${dir}.tmp.csv > results_${dir}.csv
-  awk -F, ${AWKPROG} results_${dir}.tmp.csv > results_${dir}.csv
-#  rm results_${dir}.tmp.csv
+  rsync -a --progress splunker@${TESTBOX_HOST}:./results/$dir/results.csv results_${dir}.tmp.csv
+  awk -F, "${AWKPROG}" results_${dir}.tmp.csv > results_${dir}.csv
+  rm results_${dir}.tmp.csv
+  echo Created: results_${dir}.csv
 done
