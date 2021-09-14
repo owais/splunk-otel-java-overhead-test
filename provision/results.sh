30c693eaa790fef06e433aee0666664c10da31ce
Jason Plumb <jplumb@splunk.com>
2021-09-14 15:44:44 -0700

    remove debug

--- provision/results.sh
+++ provision/results.sh
@@ -7,8 +7,6 @@ source env.sh
 FIELD_SEQ=$(for f in `seq 0 15` ; do for a in `seq 0 5` ; do echo -n "\$$(($f+2+15*$a)) \",\" "; done; done )
 AWKPROG=$(echo "{ print \$1 \",\" ${FIELD_SEQ} }" | sed -e "s/.....}/}/")
 
-echo $AWKPROG
-
 for dir in $( ssh splunker@${TESTBOX_HOST} ls results) ; do
   rsync -avv --progress splunker@${TESTBOX_HOST}:./results/$dir/results.csv results_${dir}.tmp.csv
   echo awk -F, ${AWKPROG} results_${dir}.tmp.csv > results_${dir}.csv
