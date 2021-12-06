965a77d615bec171aa9b676c7cb69905182e58e2
Jason Plumb <jplumb@splunk.com>
2021-12-06 14:52:33 -0800

    don't sort -- print in the order given

--- src/test/java/io/opentelemetry/results/PrintStreamPersister.java
+++ src/test/java/io/opentelemetry/results/PrintStreamPersister.java
@@ -8,11 +8,9 @@ import io.opentelemetry.config.TestConfig;
 
 import java.io.PrintStream;
 import java.time.Duration;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
@@ -24,38 +22,36 @@ class PrintStreamPersister implements ResultsPersister {
 
   @Override
   public void write(List<AppPerfResults> results) {
-    List<AppPerfResults> sorted = results.stream()
-        .sorted(Comparator.comparing(AppPerfResults::getAgentName)).collect(Collectors.toList());
-    TestConfig config = sorted.stream().findFirst().get().config;
+    TestConfig config = results.stream().findFirst().get().config;
     out.println("----------------------------------------------------------");
     out.println(" Run at " + new Date());
     out.printf(" %s : %s\n", config.getName(), config.getDescription());
     out.printf(" %d users, %d iterations\n", config.getConcurrentConnections(), config.getTotalIterations());
     out.println("----------------------------------------------------------");
 
-    display(sorted, "Agent", appPerfResults -> appPerfResults.agent.getName());
-    display(sorted, "Run duration", res -> {
+    display(results, "Agent", appPerfResults -> appPerfResults.agent.getName());
+    display(results, "Run duration", res -> {
       Duration duration = Duration.ofMillis(res.runDurationMs);
       return String.format("%02d:%02d:%02d", duration.toHours(), duration.toMinutesPart(), duration.toSecondsPart());
     });
-    display(sorted, "Avg. CPU (user)", res -> String.valueOf(res.averageJvmUserCpu));
-    display(sorted, "Max. CPU (user)", res -> String.valueOf(res.maxJvmUserCpu));
-    display(sorted, "Avg. mch tot cpu", res -> String.valueOf(res.averageMachineCpuTotal));
-    display(sorted, "Startup time (ms)", res -> String.valueOf(res.startupDurationMs));
-    display(sorted, "Total allocated MB", res -> format(res.getTotalAllocatedMB()));
-    display(sorted, "Heap (min)", res -> String.valueOf(res.heapUsed.min));
-    display(sorted, "Heap (max)", res -> String.valueOf(res.heapUsed.max));
-    display(sorted, "Thread switch rate",
+    display(results, "Avg. CPU (user)", res -> String.valueOf(res.averageJvmUserCpu));
+    display(results, "Max. CPU (user)", res -> String.valueOf(res.maxJvmUserCpu));
+    display(results, "Avg. mch tot cpu", res -> String.valueOf(res.averageMachineCpuTotal));
+    display(results, "Startup time (ms)", res -> String.valueOf(res.startupDurationMs));
+    display(results, "Total allocated MB", res -> format(res.getTotalAllocatedMB()));
+    display(results, "Heap (min)", res -> String.valueOf(res.heapUsed.min));
+    display(results, "Heap (max)", res -> String.valueOf(res.heapUsed.max));
+    display(results, "Thread switch rate",
         res -> String.valueOf(res.maxThreadContextSwitchRate));
-    display(sorted, "GC time", res -> String.valueOf(res.totalGCTime));
-    display(sorted, "GC pause time", res -> String.valueOf(NANOSECONDS.toMillis(res.totalGcPauseNanos)));
-    display(sorted, "Req. mean", res -> format(res.requestAvg));
-    display(sorted, "Req. p95", res -> format(res.requestP95));
-    display(sorted, "Iter. mean", res -> format(res.iterationAvg));
-    display(sorted, "Iter. p95", res -> format(res.iterationP95));
-    display(sorted, "Net read avg (bps)", res -> format(res.averageNetworkRead));
-    display(sorted, "Net write avg (bps)", res -> format(res.averageNetworkWrite));
-    display(sorted, "Peak threads", res -> String.valueOf(res.peakThreadCount));
+    display(results, "GC time", res -> String.valueOf(res.totalGCTime));
+    display(results, "GC pause time", res -> String.valueOf(NANOSECONDS.toMillis(res.totalGcPauseNanos)));
+    display(results, "Req. mean", res -> format(res.requestAvg));
+    display(results, "Req. p95", res -> format(res.requestP95));
+    display(results, "Iter. mean", res -> format(res.iterationAvg));
+    display(results, "Iter. p95", res -> format(res.iterationP95));
+    display(results, "Net read avg (bps)", res -> format(res.averageNetworkRead));
+    display(results, "Net write avg (bps)", res -> format(res.averageNetworkWrite));
+    display(results, "Peak threads", res -> String.valueOf(res.peakThreadCount));
   }
 
   private void display(List<AppPerfResults> results, String pref,
