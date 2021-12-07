44016bce9c6cc94e5e603a781b8544b047466a74
Jason Plumb <jplumb@splunk.com>
2021-12-06 19:54:01 -0800

    add some progress/status updates via files.

--- src/test/java/io/opentelemetry/OverheadWithExternalsTests.java
+++ src/test/java/io/opentelemetry/OverheadWithExternalsTests.java
@@ -29,6 +29,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -55,6 +56,7 @@ public class OverheadWithExternalsTests {
     runDurations.clear();
     config.getAgents().forEach(agent -> {
       try {
+        logProgress(config, agent);
         runAppOnce(config, agent);
       } catch (Exception e) {
         fail("Unhandled exception in " + config.getName(), e);
@@ -62,6 +64,37 @@ public class OverheadWithExternalsTests {
     });
     List<AppPerfResults> results = new ResultsCollector(namingConventions.local, runDurations).collect(config);
     new MainResultsPersister(config).write(results);
+  }
+
+  private void logProgress(TestConfig config, Agent agent) {
+    List<TestConfig> configs = Configs.all().collect(Collectors.toList());
+    int totalConfigs = configs.size();
+    int thisConfigNum = configs.indexOf(config);
+    int agents = config.getAgents().size();
+    int thisAgentNum = config.getAgents().indexOf(agent);
+
+    int totalAgentRunsAcrossAllConfigs = Configs.all().reduce(0, (integer, testConfig) -> testConfig.getAgents().size(), Integer::sum);
+    int currentPosition = 0;
+    for (TestConfig testConfig : configs) {
+      List<Agent> thisConfigAgents = testConfig.getAgents();
+      for (Agent thisConfigAgent : thisConfigAgents) {
+        currentPosition++;
+        if((testConfig == config) && (thisConfigAgent == agent)){
+          break;
+        }
+      }
+    }
+    String output = String.format("Config %d/%d Agent %d/%d - Total %d/%d\n", thisConfigNum + 1, totalConfigs, thisAgentNum + 1, agents, currentPosition, totalAgentRunsAcrossAllConfigs);
+    System.out.printf(output);
+    writeProgress(output);
+  }
+
+  private void writeProgress(String output) {
+    try {
+      Files.writeString(Path.of("/tmp/progress.txt"), output);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   void runAppOnce(TestConfig config, Agent agent) throws Exception {
