650aff4a795ef4c717ae6d1097578b914fb5e46a
jason plumb <75337021+breedx-splk@users.noreply.github.com>
2022-01-03 10:21:33 -0800

    Update web/overhead.js
    Co-authored-by: Fabrizio Ferri-Benedetti <fferribenedetti@splunk.com>

--- web/overhead.js
+++ web/overhead.js
@@ -31,7 +31,7 @@ function populateRunsDropDown(runNames) {
 
 function addCharts(aggregated) {
     makeChart(aggregated, 'startupDurationMs', "seconds", x => x / 1000);
-    makeChart(aggregated, 'averageCpuUser', "percent (%)");
+    makeChart(aggregated, 'averageCpuUser', "% CPU load");
     makeChart(aggregated, 'maxCpuUser', "% CPU load");
     makeChart(aggregated, 'maxHeapUsed', "megabytes", x => x / (1024 * 1024));
     makeChart(aggregated, 'totalAllocatedMB', "Gigabytes", x => x / (1024));
