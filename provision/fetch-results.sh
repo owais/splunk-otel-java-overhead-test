13343369d6a847e1deefd05a3fb86a70bca25628
Jason Plumb <jplumb@splunk.com>
2022-01-27 14:12:55 -0800

    fetch results into consistent location

--- provision/fetch-results.sh
+++ provision/fetch-results.sh
@@ -3,7 +3,6 @@
 # fetches the remote results. Assumes there is only one results directory.
 
 MYDIR=$(dirname $0)
-
 source ${MYDIR}/env.sh
 
 RDIR=$(ssh -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" -o "LogLevel=ERROR" -i ~/.orca/id_rsa  splunk@${TESTBOX_HOST} ls results)
@@ -11,6 +10,9 @@ echo Remote dir is ${RDIR}
 TS=$(ssh -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" -o "LogLevel=ERROR" -i ~/.orca/id_rsa  splunk@${TESTBOX_HOST} "date -r results/${RDIR} '+%Y%m%d_%H%M%S'")
 echo Timestamp dir will be ${TS}
 
+RESULTS=${MYDIR}/../results/${TS}
+mkdir -p $RESULTS
+
 rsync -avv --progress -e \
   'ssh -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" -o "LogLevel=ERROR" -i ~/.orca/id_rsa' \
-   "splunk@${TESTBOX_HOST}:results/${RDIR}/" "${TS}/"
+   "splunk@${TESTBOX_HOST}:results/${RDIR}/" "${RESULTS}/"
