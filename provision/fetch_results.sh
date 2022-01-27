10c82ddf64062157688063bdd93dccac2acd424f
Jason Plumb <jplumb@splunk.com>
2022-01-27 13:54:08 -0800

    add script to fetch results

---
+++ provision/fetch_results.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# fetches the remote results. Assumes there is only one results directory.
+
+MYDIR=$(dirname $0)
+
+source ${MYDIR}/env.sh
+
+RDIR=$(ssh -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" -o "LogLevel=ERROR" -i ~/.orca/id_rsa  splunk@${TESTBOX_HOST} ls results)
+echo Remote dir is ${RDIR}
+TS=$(ssh -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" -o "LogLevel=ERROR" -i ~/.orca/id_rsa  splunk@${TESTBOX_HOST} "date -r results/${RDIR} '+%Y%m%d_%H%M%S'")
+echo Timestamp dir will be ${TS}
+
+rsync -avv --progress -e \
+  'ssh -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" -o "LogLevel=ERROR" -i ~/.orca/id_rsa' \
+   "splunk@${TESTBOX_HOST}:results/${RDIR}/" "${TS}/"
